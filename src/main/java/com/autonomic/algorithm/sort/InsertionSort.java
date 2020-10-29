c0df45861ed71c4094b3c26e8494f7f2b39238d8
sxh <xuhang_shang@163.com>
2020-10-30 00:08:18 +0800

    插入、希尔排序复习

--- src/main/java/com/autonomic/algorithm/sort/InsertionSort.java
+++ src/main/java/com/autonomic/algorithm/sort/InsertionSort.java
@@ -2,7 +2,6 @@ package com.autonomic.algorithm.sort;/**
  * Created by SXH on 2020/5/14 22:13.
  */
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 /**
@@ -12,23 +11,41 @@ import java.util.Arrays;
  */
 public class InsertionSort {
 	public static void main(String[] args) {
-//		int [] arr1 = {103, 80 ,60, 70, 50, -1, 90, 66, 3, -10,90};
-//		System.out.println(Arrays.toString(arr1));
+		int [] arr1 = {103, 80 ,60, 70, 50, -1, 90, 66, 3, -10,90};
+		System.out.println(Arrays.toString(arr1));
 //		sort(arr1);
-//		System.out.println(Arrays.toString(arr1));
+		practiceSort(arr1);
+		System.out.println(Arrays.toString(arr1));
 
 		// 创建80000个随机数的数组
-		int MAX_SIZE = 80000;
-		int [] arr = new int[MAX_SIZE];
-		for (int i = 0; i < MAX_SIZE; i++) {
-			arr[i] = (int)(Math.random() * 8000000);
+//		int MAX_SIZE = 80000;
+//		int [] arr = new int[MAX_SIZE];
+//		for (int i = 0; i < MAX_SIZE; i++) {
+//			arr[i] = (int)(Math.random() * 8000000);
+//		}
+//		long start = System.currentTimeMillis();
+//		System.out.println(start);
+//		sort(arr);
+//		long end = System.currentTimeMillis();
+//		System.out.println(end);
+//		System.out.println((end - start)); // 八万数据大概耗时 600ms
+	}
+
+	/**
+	 * my practice
+	 * 左边有序表  右边无序表，从第二个开始（也就是index=1），index=0 为初始有序表
+	 * 效率降低50%
+	 * @date 2020/10/29 22:53.
+	 */
+	public static void practiceSort(int [] arr) {
+		int len = arr.length;
+		for (int i = 1; i < len; i++) {
+			for (int j = i; j > 0 && arr[j] < arr[j-1]; j--) {
+				int temp = arr[j];
+				arr[j] = arr[j-1];
+				arr[j-1] = temp;
+			}
 		}
-		long start = System.currentTimeMillis();
-		System.out.println(start);
-		sort(arr);
-		long end = System.currentTimeMillis();
-		System.out.println(end);
-		System.out.println((end - start)); // 八万数据大概耗时 600ms
 	}
 
 	/**
@@ -43,12 +60,17 @@ public class InsertionSort {
 		int insertVal = 0; // 待插入的值
 		int insertIndex = 0; // 插入的下标
 		for (int i = 1, len = arr.length; i < len; i++) { // 从第二个数开始比较，第一个数默认放入到有序队列
+			// 暂存每次从无序表里面拿出要放入有序表中的数
 			insertVal = arr[i];
+			// 初始要插入的下标
 			insertIndex = i - 1;
 			// 第一个比较是防止下标越界
 			// 第二个比较（从小到大排序） 比较要插入的值和插入下标对应的值，小了就交换双方的值向前然后继续循环
 			while (insertIndex >= 0 && insertVal < arr[insertIndex]) {
+				// 满足while的条件，表明有序表中的那个值大于从无序表中拿出来的值，
+				// 那么这个无序表中的值就还没有找到它的位置，需要将有序表的那个值向后移动一位
 				arr[insertIndex + 1] = arr[insertIndex];
+				// 并且继续拿无序表中的那个值和有序表的前一个值对比，（想定于从后向前遍历找无序表那个数的下标）
 				insertIndex--;
 			}
 			// 判断如果需要插入的位置和自己本身的位置一样，就不需要进行赋值操作了
--- src/main/java/com/autonomic/algorithm/sort/ShellSort.java
+++ src/main/java/com/autonomic/algorithm/sort/ShellSort.java
@@ -7,27 +7,27 @@ import java.util.Arrays;
 /**
  * @author SXH
  * @description 希尔排序  也是一种插入排序，它是简单插入排序经过改进之后的一个更高效的版本，也称为缩小增量排序
- *  希尔排序是吧记录按下标的一定增量分组，对每组使用直接插入排序算法排序；随着增量逐渐减少，每组包含的关键词越来越多，
+ *  希尔排序是把记录按下标的一定增量分组，对每组使用直接插入排序算法排序；随着增量逐渐减少，每组包含的关键词越来越多，
  *  当增量减至1时，整个文件恰被分成一组，算法便终止
  * @date 2020/5/18 22:58.
  */
 public class ShellSort {
 	public static void main(String[] args) {
-//		int [] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
-//		sort2(arr);
+		int [] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+		sort2(arr);
 
 		// 创建80000个随机数的数组
-		int MAX_SIZE = 800000;
-		int [] arr = new int[MAX_SIZE];
-		for (int i = 0; i < MAX_SIZE; i++) {
-			arr[i] = (int)(Math.random() * 8000000);
-		}
-		long start = System.currentTimeMillis();
-		System.out.println(start);
-		sort2(arr);
-		long end = System.currentTimeMillis();
-		System.out.println(end);
-		System.out.println((end - start)); // 八十万数据大概耗时 165ms
+//		int MAX_SIZE = 80000;
+//		int [] arr = new int[MAX_SIZE];
+//		for (int i = 0; i < MAX_SIZE; i++) {
+//			arr[i] = (int)(Math.random() * 8000000);
+//		}
+//		long start = System.currentTimeMillis();
+//		System.out.println(start);
+//		sort1(arr);
+//		long end = System.currentTimeMillis();
+//		System.out.println(end);
+//		System.out.println((end - start)); // 八十万数据大概耗时 165ms
 	}
 
 
@@ -108,7 +108,7 @@ public class ShellSort {
 					arr[j] = temp;
 				}
 			}
-//			System.out.println(Arrays.toString(arr));
+			System.out.println(Arrays.toString(arr));
 		}
 	}
 }
