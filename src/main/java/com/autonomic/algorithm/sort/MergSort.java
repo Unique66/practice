a99104157ee5de4cd3fcd8f9e4bb536e7947a817
sxh <xuhang_shang@163.com>
2020-11-18 23:46:18 +0800

    归并排序复习

--- src/main/java/com/autonomic/algorithm/sort/MergSort.java
+++ src/main/java/com/autonomic/algorithm/sort/MergSort.java
@@ -13,10 +13,10 @@ import java.util.Arrays;
  */
 public class MergSort {
 	public static void main(String[] args) {
-		int [] arr = {8, 4, 5, 7, 1, 3, 6, 2};
-		int [] temp = new int[arr.length]; // 归并排序需要一个额外的空间（空间换时间）
-		mergeSort(arr, 0, arr.length - 1, temp);
-		System.out.println("归并排序后：" + Arrays.toString(arr));
+//		int [] arr = {8, 4, 5, 7, 1, 3, 6, 2};
+//		int [] temp = new int[arr.length]; // 归并排序需要一个额外的空间（空间换时间）
+//		mergeSort(arr, 0, arr.length - 1, temp);
+//		System.out.println("归并排序后：" + Arrays.toString(arr));
 
 		// 创建80000个随机数的数组
 //		int MAX_SIZE = 800000;
@@ -31,7 +31,69 @@ public class MergSort {
 //		long end = System.currentTimeMillis();
 //		System.out.println(end);
 //		System.out.println((end - start)); // 八万数据大概耗时 19ms
+
+
+		// 测试练习
+//		int [] arr = {1, 3, 5, 7, 2, 4, 6, 8};
+		int [] arr = {8, 4, 5, 7, 1, 3, 6, 2};
+		int [] temp = new int[arr.length]; // 归并排序需要一个额外的空间（空间换时间）
+//		int mid = (0 + arr.length-1) / 2;
+//		mergeTest1(arr, 0, mid, arr.length-1, temp);
+		sortTest1(arr, 0, arr.length - 1, temp);
+		System.out.println("归并排序后：" + Arrays.toString(arr));
 	}
+
+	/**************************** 2020/11/18 练习 *********************************/
+	// 归并排序分为拆分数组 和 合并有序数组两个过程
+	// 拆分数组
+	// 归并排序是空间换时间的一种思想，所以需要用一个temp 数组来暂存两个有序数组合并后的数组
+	public static void sortTest1(int [] arr, int l, int r, int [] temp) {
+		if (l < r) {
+			int mid = (l + r) / 2;
+			// 向左拆分
+			sortTest1(arr, l, mid, temp);
+			// 向右拆分
+			sortTest1(arr, mid+1, r, temp);
+			// 拆分为一个个单个数组后，将单个数组（此时有序）再一个个的合并
+			mergeTest1(arr, l, mid, r, temp);
+		}
+	}
+
+	// 合并代码
+	public static void mergeTest1(int [] arr, int l, int mid, int r, int [] temp) {
+		// 1.将左右两个有序数组合并到temp 中，保证合并后的temp 数组还是有序的
+		int tempIndex = 0; // 记录temp 数组的下标位置
+		int left = l; // 用来记录处理左边数组的元素的情况
+		int m = mid + 1; // 用来记录处理右边数组的元素的情况
+		while (left <= mid && m <= r) { // 当都满足时
+			// 判断两个数组 两边待处理的第一个元素的大小
+			if (arr[left] > arr[m]) {
+				temp[tempIndex++] = arr[m++];
+			} else {
+				temp[tempIndex++] = arr[left++];
+			}
+		}
+
+		// 2.由于将其中一个数组完全的放入temp 数组后，另外一个数组可能还有好几个数没有放入temp 数组，
+		// 所以后序要加此判断，将另外一个没有处理完毕的数组的元素也添加到temp 数组中
+		while (left <= mid) {
+			// 说明左边的数组没有全部放入到temp 数组中呢
+			temp[tempIndex++] = arr[left++];
+		}
+		while (m <= r) {
+			// 说明右边的数组没有全部放入到temp 数组中呢
+			temp[tempIndex++] = arr[m++];
+		}
+
+		// 3.都放入到了temp 数组中后，就需要将temp 数组中的元素依次放回到arr 数组中去
+		int index = 0; // 用于处理temp 数组中的元素
+		for (int i = l; i <= r; i++) {
+			arr[i] = temp[index++];
+		}
+	}
+
+	/*********************************************************************************/
+
 
 	// 分解 并 合并
 	public static void mergeSort(int [] arr, int left, int right, int [] temp) {
