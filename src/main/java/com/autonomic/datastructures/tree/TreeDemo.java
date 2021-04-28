bdd059618fd6dcb9b5743f46943ae7073a488d08
sxh <xuhang_shang@163.com>
2021-04-29 00:04:50 +0800

    1.通过链表递归生成二叉树 2.非递归（栈）实现前序遍历 3.使用队列实现层序遍历

--- src/main/java/com/autonomic/datastructures/tree/TreeDemo.java
+++ src/main/java/com/autonomic/datastructures/tree/TreeDemo.java
@@ -5,6 +5,11 @@
 
 package com.autonomic.datastructures.tree;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * @author SXH
  * @description 树结构   前序、中序、后序遍历
@@ -20,15 +25,16 @@ public class TreeDemo {
         TreeNode root = new TreeNode(1, "宋江");
         root.setLeft(new TreeNode(2, "吴用"));
         root.setRight(new TreeNode(3, "卢俊义"));
-        root.getRight().setRight(new TreeNode(4, "林冲"));
-        root.getRight().setLeft(new TreeNode(5, "关胜"));
+        root.getLeft().setLeft(new TreeNode(4, "4"));
+        root.getLeft().setRight(new TreeNode(5, "5"));
+        root.getRight().setRight(new TreeNode(6, "林冲"));
 
         tree.setRoot(root);
-
+        System.out.println("--------递归前序遍历$$$$$$$$$$$$$$$$$$$$");
         tree.preList();
-        System.out.println("$$$$$$$$$$$$$$$$$$$$");
+        System.out.println("--------递归中序遍历$$$$$$$$$$$$$$$$$$$$");
         tree.innerList();
-        System.out.println("$$$$$$$$$$$$$$$$$$$$");
+        System.out.println("--------递归后序遍历$$$$$$$$$$$$$$$$$$$$");
         tree.subList();
 
         int no = 5;
@@ -37,11 +43,20 @@ public class TreeDemo {
         System.out.println(treeNode);
 
         System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&");
-        tree.preList();
-        tree.delNode(5);
+//        tree.preList();
+//        tree.delNode(5);
         System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&");
         tree.preList();
 
+        // test createBinaryTree()
+        LinkedList<Integer> inputList = new LinkedList<>(Arrays.asList(3, 2, 9, null, null, 10, null, null, 8, null, 4));
+        TreeNode treeRoot = Tree.createBinaryTree(inputList);
+
+        System.out.println("--------非递归前序遍历$$$$$$$$$$$$$$$$$$$$");
+        tree.preOrderWithStack();
+
+        System.out.println("--------层序遍历$$$$$$$$$$$$$$$$$$$$");
+        tree.floorList();
     }
 }
 
@@ -60,6 +75,14 @@ class Tree {
         }
     }
 
+    public void preOrderWithStack() {
+        if (this.root != null) {
+            this.root.preOrderWithStack();
+        } else {
+            System.out.println("树空");
+        }
+    }
+
     public void innerList() {
         if (this.root != null) {
             this.root.innerList();
@@ -71,6 +94,15 @@ class Tree {
     public void subList() {
         if (this.root != null) {
             this.root.subList();
+        } else {
+            System.out.println("树空");
+        }
+    }
+
+    // 广度优先-层序遍历
+    public void floorList() {
+        if (this.root != null) {
+            this.root.floorList();
         } else {
             System.out.println("树空");
         }
@@ -111,7 +143,6 @@ class Tree {
         if (this.root != null) {
             if (this.root.getId() == no) {
                 this.root = null;
-                return;
             } else {
                 this.root.delNode(no);
             }
@@ -119,10 +150,31 @@ class Tree {
             System.out.println("空树，无法delNode");
         }
     }
+
+    /**
+     * 使用链表结构递归创建逻辑树结构
+     *
+     * @return 根节点
+     * @param: inputList
+     */
+    public static TreeNode createBinaryTree(LinkedList<Integer> inputList) {
+        TreeNode node = null;
+        if (inputList == null || inputList.isEmpty()) {
+            return null;
+        }
+        Integer data = inputList.removeFirst();
+        // 如果一个元素是空，则不再进一步递归
+        if (data != null) {
+            node = new TreeNode(data, data.toString());
+            node.setLeft(createBinaryTree(inputList));
+            node.setRight(createBinaryTree(inputList));
+        }
+        return node;
+    }
 }
 
 class TreeNode {
-    private int id;
+    private Integer id;
     private String name;
     private TreeNode left;
     private TreeNode right;
@@ -133,11 +185,11 @@ class TreeNode {
         this.name = name;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -184,6 +236,25 @@ class TreeNode {
         }
     }
 
+    // 前序遍历，非递归方式
+    public void preOrderWithStack() {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode treeNode = this;
+        while (treeNode != null || !stack.isEmpty()) {
+            // 迭代访问节点的左孩子，并入栈
+            while (treeNode != null) {
+                System.out.println(treeNode.id);
+                stack.push(treeNode);
+                treeNode = treeNode.left;
+            }
+            // 如果节点没有左孩子，则弹出栈顶节点，访问节点右孩子
+            if (!stack.isEmpty()) {
+                treeNode = stack.pop();
+                treeNode = treeNode.right;
+            }
+        }
+    }
+
     // 中序遍历  左节点、根节点、右节点
     public void innerList() {
         if (this.left != null) {
@@ -204,6 +275,22 @@ class TreeNode {
             this.right.subList();
         }
         System.out.println(this);
+    }
+
+    // 层序遍历，利用队列实现
+    public void floorList() {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(this);
+        while (!queue.isEmpty()) {
+            TreeNode poll = queue.poll();
+            System.out.println(poll.id);
+            if (poll.getLeft() != null) {
+                queue.offer(poll.getLeft());
+            }
+            if (poll.getRight() != null) {
+                queue.offer(poll.getRight());
+            }
+        }
     }
 
     /***
@@ -232,8 +319,9 @@ class TreeNode {
 
     /**
      * 中序查找
-     * @param: no 待查找的节点 id
+     *
      * @return TreeNode 如果查找到，返回  没有找到返回空
+     * @param: no 待查找的节点 id
      */
     public TreeNode innerFind(int no) {
         TreeNode resultNode = null;
@@ -257,8 +345,9 @@ class TreeNode {
 
     /**
      * 后序查找
-     * @param: no 待查找的节点 id
+     *
      * @return TreeNode 如果查找到，返回  没有找到返回空
+     * @param: no 待查找的节点 id
      */
     public TreeNode subFind(int no) {
         TreeNode resultNode = null;
@@ -290,13 +379,14 @@ class TreeNode {
      * 规定：
      * 1）如果删除的节点是叶子节点，则删除该节点
      * 2）如果删除的节点是非叶子节点，则删除该子树
-     *
+     * <p>
      * 思路：
      * 1.首先判断根节点是否为空，为空则报树为空无法删除，不为空，判断根节点no 与待删除no 是否一致，一致删除整个树，不一致向下走
      * 2.判断左节点是否不为空并且左节点no == 待删除no ，删除并return
      * 3.同理判断右节点是否不为空并且节点no == 待删除节点，相等则删除并return
      * 4.走到这步说明左右都没有找到待删除的节点，那么就拿节点向左递归
      * 5.左递归没有找到，那么就右递归
+     *
      * @param no 待删除节点ID
      */
 
