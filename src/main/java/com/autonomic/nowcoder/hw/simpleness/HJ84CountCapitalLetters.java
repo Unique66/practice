bbb77d87ac2a65d53e7d995d43c64d6d42ce9551
sxh <xuhang_shang@163.com>
2021-07-14 00:32:57 +0800

    【算法】【NowCoder】HWSimpleness待做：     HJ84 统计大写字母个数     HJ85 最长回文子串     HJ86 求最大连续bit数     HJ87 密码强度等级

---
+++ src/main/java/com/autonomic/nowcoder/hw/simpleness/HJ84CountCapitalLetters.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2021.  User:Unique66 File:HJ84CountCapitalLetters.java
+ * Date:2021/07/14 00:31:14
+ */
+
+package com.autonomic.nowcoder.hw.simpleness;
+
+/**
+ * @author Unique66
+ * @description HJ84 统计大写字母个数
+ * @date 2021/7/14 0:31
+ *
+ * https://www.nowcoder.com/practice/434414efe5ea48e5b06ebf2b35434a9c?tpId=37&&tqId=21307&rp=1&ru=/ta/huawei&qru=/ta/huawei/question-ranking
+ * 描述
+ * 找出给定字符串中大写字符(即'A'-'Z')的个数。
+ *
+ * 输入描述：
+ * 本题含有多组样例输入
+ * 对于每组样例，输入一行，代表待统计的字符串
+ *
+ * 输出描述：
+ * 对于每组样例，输出一个整数，代表字符串中大写字母的个数
+ *
+ * 示例1
+ * 输入：
+ * add123#$%#%#O
+ * 150175017(&^%&$vabovbao
+ * 输出：
+ * 1
+ * 0
+ */
+public class HJ84CountCapitalLetters {
+}
---
+++ src/main/java/com/autonomic/nowcoder/hw/simpleness/HJ85LongestPalindromicSubString.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2021.  User:Unique66 File:HJ85LongestPalindromicSubString.java
+ * Date:2021/07/14 00:29:14
+ */
+
+package com.autonomic.nowcoder.hw.simpleness;
+
+/**
+ * @author Unique66
+ * @description HJ85 最长回文子串
+ * @date 2021/7/14 0:29
+ *
+ * https://www.nowcoder.com/practice/12e081cd10ee4794a2bd70c7d68f5507?tpId=37&&tqId=21308&rp=1&ru=/ta/huawei&qru=/ta/huawei/question-ranking
+ * 描述
+ * 给定一个仅包含小写字母的字符串，求它的最长回文子串的长度。
+ * 所谓回文串，指左右对称的字符串。
+ * 所谓子串，指一个字符串删掉其部分前缀和后缀（也可以不删）的字符串
+ * （注意：记得加上while处理多个测试用例）
+ *
+ * 输入描述：
+ * 输入一个仅包含小写字母的字符串
+ *
+ * 输出描述：
+ * 返回最长回文子串的长度
+ *
+ * 示例1
+ * 输入：
+ * cdabbacc
+ * 输出：
+ * 4
+ * 说明：
+ * abba为最长的回文子串
+ */
+public class HJ85LongestPalindromicSubString {
+}
