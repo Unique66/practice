0b8aad3b8699274b313b8598887d1e847773f6e8
Unique66 <xuhang_shang@163.com>
2022-12-07 22:50:05 +0800

    【算法】【LeetCode】Simpleness 21 合并两个有序链表

---
+++ src/main/java/com/autonomic/leetcode/simpleness/LeetCode21.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2022.  User:Unique66 File:LeetCode21.java
+ * Date:2022/12/04 19:55:04
+ */
+
+package com.autonomic.leetcode.simpleness;
+
+/**
+ * 合并两个有序链表
+ *
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ *
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ *
+ * 示例 2：
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ *
+ * 示例 3：
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/merge-two-sorted-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode21 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        // 创建头结点，作为输出
+        ListNode result = new ListNode();
+        // 使用临时节点作为数据拼接的桥梁
+        ListNode temp = result;
+        while (list1 != null && list2 != null) {
+            if (list1.val > list2.val) {
+                temp.next = list2;
+                list2 = list2.next;
+            } else {
+                temp.next = list1;
+                list1 = list1.next;
+            }
+            temp = temp.next;
+        }
+        // while 循环完后，可能还有一个链表的数据没有拼接完，需要续上
+        if (list1 != null) {
+            temp.next = list1;
+        }
+        if (list2 != null) {
+            temp.next = list2;
+        }
+        return result.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
